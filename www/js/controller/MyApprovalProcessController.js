8feaf7c9e2c577e7bed4f8f0d48da85a165b24f3
heyh <yhua.he@163.com>
2017-07-25 16:10:51 +0800

    push

--- www/js/controller/MyApprovalProcessController.js
+++ www/js/controller/MyApprovalProcessController.js
@@ -12,6 +12,8 @@ angular.module('myApprovalProcess.controllers', ['MyApprovalProcess.services'])
 
         var uid = JSON.parse(localStorage.getUser())["uid"];
         var cid = JSON.parse(localStorage.getItem("company"))["cid"];
+        var rightList = new Array(JSON.parse(localStorage.getItem("right")).rightList);
+        var parentId = JSON.parse(localStorage.getItem("right")).parentId;
 
         ////////////////////////////////////////////////////////////////////
         var pageData = $scope.pageData = {
@@ -20,7 +22,11 @@ angular.module('myApprovalProcess.controllers', ['MyApprovalProcess.services'])
             pagination: {
                 limitSize: 5,
                 currentPage: 1
-            }
+            },
+
+            hasOnlyReadRight700: JSON.stringify(rightList).indexOf("16")!=-1 && 0 != parentId,
+            hasReadEditRight700: JSON.stringify(rightList).indexOf("15")!=-1 || 0 == parentId,
+            hasOutRight: JSON.stringify(rightList).indexOf("17")!=-1
         };
 
         $scope.getMyApprovalDataList = function () {
@@ -63,6 +69,36 @@ angular.module('myApprovalProcess.controllers', ['MyApprovalProcess.services'])
             } else {
                 $scope.pageData.moreData = false;
             }
+
+            angular.forEach(data.myApprovalDataList, function (item) {
+                var prefixItemCode = item.itemCode.substring(0, 3);
+
+                if (prefixItemCode == '000' || prefixItemCode > 900) {  // 资料
+                    item.fieldDataTypeName = '项目资料';
+                    item.fieldDataTypeCode = 'doc';
+                } else if (prefixItemCode == '700') {                   // 清单
+                    item.fieldDataTypeName = '清单项量';
+                    item.fieldDataTypeCode = 'bill';
+                } else if (prefixItemCode == '800') {                   // 材料
+                    item.fieldDataTypeName = '项目材料';
+                    item.fieldDataTypeCode = 'material';
+                } else{                                                 // 数据
+                    item.fieldDataTypeName = '项目数据';
+                    item.fieldDataTypeCode = 'data';
+                }
+
+                if (pageData.hasOutRight && item.itemCode.substring(0, 3) == '800') {
+                    item.price = '***';
+                } else if(item.itemCode.substring(0, 3) == '700' && !pageData.hasOnlyReadRight700 && !pageData.hasReadEditRight700) {
+                    item.price = '***';
+                }
+                item.money = item.price != '***' ? (item.count * item.price).toFixed(2) : '***';
+
+                item.money_ys = item.price_ys != '' ? (item.count * item.price_ys).toFixed(2) : '0.00';
+                item.money_sj = item.price_sj != '' ? (item.count * item.price_sj).toFixed(2) : '0.00';
+
+            });
+
             if (refresh) {
                 $scope.pageData.myApprovalDataList = data.myApprovalDataList;
             } else {
