b6c8df7f14d9c4cd785f331eac2d72ee930aa90a
heyh <yhua.he@163.com>
2017-10-18 19:51:11 +0800

    push

--- www/js/controller/MyApprovalDetailsController.js
+++ www/js/controller/MyApprovalDetailsController.js
@@ -4,7 +4,7 @@
 
 
 angular.module('myApprovalDetails.controllers', ['MyApprovalDetails.services'])
-    .controller('MyApprovalDetailsCtrl', function (MyApprovalDetailsService, DataDetailsService, $scope, $state, $ionicModal, $ionicViewSwitcher, $stateParams, fileUtil, $ionicPlatform) {
+    .controller('MyApprovalDetailsCtrl', function (MyApprovalDetailsService, DataDetailsService, $scope, $state, $ionicModal, $ionicViewSwitcher, $stateParams, fileUtil, $ionicPlatform, $compile) {
 
         $scope.goBack = function () {
             $ionicViewSwitcher.nextDirection('back');
@@ -13,9 +13,40 @@ angular.module('myApprovalDetails.controllers', ['MyApprovalDetails.services'])
 
         $(function () {
             $scope._isIOS = $ionicPlatform.is('ios');
-            $scope.myApprovalData = $stateParams.myApprovalData;
+            var myApprovalData = $scope.myApprovalData = $stateParams.myApprovalData;
             $scope.myApprovalData.money = (parseFloat($scope.myApprovalData.count) * parseFloat($scope.myApprovalData.price)).toFixed(2);
+
+
+            if (myApprovalData.approvedOption != '' && myApprovalData.approvedOption != undefined && myApprovalData.approvedOption != 'undefined') {
+                var approvedOptions = [];
+                var _approvedOptions = myApprovalData.approvedOption.split('|');
+                for (var i=0; i<_approvedOptions.length; i++) {
+                    var approvedOption = {};
+                    var _approvedOptionInfos = _approvedOptions[i].split('::');
+                    approvedOption.approvedDate = _approvedOptionInfos[0];
+                    approvedOption.approvedUser = _approvedOptionInfos[1];
+                    approvedOption.approvedOption =  _approvedOptionInfos[2];
+                    approvedOptions = approvedOptions.concat(approvedOption);
+                }
+                $scope.myApprovalData.approvedOptions = approvedOptions;
+            }
         });
+
+        /**
+         * 选择单位页面
+         */
+        $ionicModal.fromTemplateUrl('templates/approvalOptionModal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.approvalOptionModal = modal;
+        });
+        $scope.openApprovedOptionModal = function () {
+            $scope.approvalOptionModal.show();
+        };
+        $scope.closeApprovedOptionModal = function () {
+            $scope.approvalOptionModal.hide();
+        };
 
         $scope.isData = function (itemCode) {
             return itemCode.substring(0, 3) != '000' && itemCode.substring(0, 3) <= 900;
